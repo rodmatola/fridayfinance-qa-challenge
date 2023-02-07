963978d99fc046d672959db4efe99efe4bff9968
rodmatola <rodmatola@gmail.com>
2023-02-07 18:53:51 -0300

    test: verify cookie storage

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.fridayfinance.com/',
+    baseUrl: 'https://www.fridayfinance.com',
+    chromeWebSecurity: false,
     hideXHRInCommandLog: true,
     scrollBehavior: 'center',
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/cookies.cy.js
+++ cypress/e2e/cookies.cy.js
@@ -1,9 +1,11 @@
 import { home } from '../support/pageElements/home.elements';
+import { acceptCookie, denyCookie } from '../fixtures/cookiesValues';
 
 describe('check cookies', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+
   it('banner must be visible and contains a link to Privacy Police and Accept and Deny buttons', () => {
     cy.get(home.cookieBanner).should('be.visible');
     cy.get(home.cookieBannerText)
@@ -11,5 +13,16 @@ describe('check cookies', () => {
       .should('have.attr', 'href', '/privacy-policy');
     cy.get(home.cookieBannerAcceptBtn).should('contain.text', 'Accept');
     cy.get(home.cookieBannerDenyBtn).should('contain.text', 'Deny');
+  });
+
+  it.only('allow cookies must contain "fs-cc" cookie with true values', () => {
+    cy.get(home.cookieBannerAcceptBtn).click();
+    cy.getCookie('fs-cc').its('value').should('contain', acceptCookie);
+  });
+
+  it.only('deny cookies must contain "fs-cc" cookie with false values', () => {
+    cy.get(home.cookieBannerDenyBtn).click();
+    cy.getCookie('fs-cc').its('value').should('contain', denyCookie);
+    cy.getCookie('fs-cc-updated').its('value').should('eq', 'true');
   });
 });
