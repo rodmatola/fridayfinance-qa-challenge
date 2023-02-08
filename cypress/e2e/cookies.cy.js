28d884e3d349135af38239d43bd1488977c8fe8b
Rodrigo Matola <rodmatola@gmail.com>
2023-02-07 21:35:36 -0300

    Check cookies automation (#2)
    * chore: installs and first configs
    
    package.json
    .nvmrc
    .gitignore
    install cypress
    cypress eslint
    cypress config
    
    * test: verify cookie banner elements
    and disble xhr logs
    
    * test: verify cookie storage
    
    * ci: add workflow yaml
    
    * fix(test): remove .only
    
    * ci: add upload artifact (video)
    
    * fix(ci): compress cypress videos

---
+++ cypress/e2e/cookies.cy.js
@@ -0,0 +1,28 @@
+import { home } from '../support/pageElements/home.elements';
+import { acceptCookie, denyCookie } from '../fixtures/cookiesValues';
+
+describe('check cookies', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('banner must be visible and contains a link to Privacy Police and Accept and Deny buttons', () => {
+    cy.get(home.cookieBanner).should('be.visible');
+    cy.get(home.cookieBannerText)
+      .find('a')
+      .should('have.attr', 'href', '/privacy-policy');
+    cy.get(home.cookieBannerAcceptBtn).should('contain.text', 'Accept');
+    cy.get(home.cookieBannerDenyBtn).should('contain.text', 'Deny');
+  });
+
+  it('allow cookies must contain "fs-cc" cookie with true values', () => {
+    cy.get(home.cookieBannerAcceptBtn).click();
+    cy.getCookie('fs-cc').its('value').should('contain', acceptCookie);
+  });
+
+  it('deny cookies must contain "fs-cc" cookie with false values', () => {
+    cy.get(home.cookieBannerDenyBtn).click();
+    cy.getCookie('fs-cc').its('value').should('contain', denyCookie);
+    cy.getCookie('fs-cc-updated').its('value').should('eq', 'true');
+  });
+});
---
+++ cypress/fixtures/cookiesValues.js
@@ -0,0 +1,5 @@
+export const acceptCookie =
+  'consents%2522%253A%257B%2522analytics%2522%253Atrue%252C%2522essential%2522%253Atrue%252C%2522marketing%2522%253Atrue%252C%2522personalization%2522%253Atrue%252C%2522uncategorized%2522%253Atrue%257D%257D';
+
+export const denyCookie =
+  'consents%2522%253A%257B%2522analytics%2522%253Afalse%252C%2522essential%2522%253Atrue%252C%2522marketing%2522%253Afalse%252C%2522personalization%2522%253Afalse%252C%2522uncategorized%2522%253Afalse%257D%257D';
